5cfed56876479d13678bbbdbc03638325c5dd187
jj131204 <jj131204@gmail.com>
2021-04-26 23:07:53 +0000

    error

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ *check_cycle - prototype
+ *
+ *@list: pointer to cycle.
+ *
+ *Return: 0 or 1
+ */
+
+int check_cycle(listint_t *list)
+{
+	if (!list)
+		return (0);
+
+	else
+		return (1);
+
+return (0);
+}
