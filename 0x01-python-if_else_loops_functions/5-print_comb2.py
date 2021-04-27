432cd133aa8372d1632e45f0b21f6eade197e68b
jj131204 <jj131204@gmail.com>
2021-04-27 21:47:35 +0000

    fix pep8

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 
-for i in range(0, 100):
-    print("{:02d}, ".format(i), end="")
+for j in range(0, 100):
+    if j != 99:
+        print("{:02d}, ".format(j), end="")
+    else:
+        print("{}".format(j), end="")
