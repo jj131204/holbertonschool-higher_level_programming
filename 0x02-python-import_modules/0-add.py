2e6b0fa3df744022c313cdef19aca2d4a0c4a917
jj131204 <jj131204@gmail.com>
2021-04-28 21:20:22 +0000

    fix

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,6 +4,4 @@ if __name__ == "__main__":
 
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b))) 
-
-#if __name__ == "__main__":
+    print("{} + {} = {}".format(a, b, add(a, b)))
