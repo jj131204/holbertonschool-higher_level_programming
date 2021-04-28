c5a54870f471dc790e39bb6bf14b5c630b606bfd
jj131204 <jj131204@gmail.com>
2021-04-28 01:07:51 +0000

    loops

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+def fizzbuzz():
+    for j in range (1, 101):
+        if j % 3 is 0 and j % 5 is 0:
+            print("FizzBuzz", end=" ")
+        elif j % 3 is 0:
+            print("Fizz", end=" ")
+        elif j % 5 is  0:
+            print("Buzz", end=" ")
+        else:
+            print(j, end=" ")
+            
