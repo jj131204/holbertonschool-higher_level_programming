b11601af9b18963d9db524dfe2136613f2cc4f05
jj131204 <jj131204@gmail.com>
2021-04-29 00:38:53 +0000

    test2

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,18 +8,17 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
+    a = int(argv[1])
+    b = int(argv[3])
+
+    if argv[2] == "+":
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif argv[2] == "-":
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif argv[2] == "*":
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    elif argv[2] == "/":
+        print("{} / {} = {}".format(a, b, div(a, b)))
     else:
-        a = argv[1]
-        u = argv[2]
-        b = argv[3]
-        if u == "+":
-            print("{} + {} = {}".format(a, b, add(a, b)))
-        elif u == "-":
-            print("{} - {} = {}".format(a, b, sub(a, b)))
-        elif u == "*":
-            print("{} * {} = {}".format(a, b, mul(a, b)))
-        elif u == "/":
-            print("{} / {} = {}".format(a, b, div(a, b)))
-        else:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
