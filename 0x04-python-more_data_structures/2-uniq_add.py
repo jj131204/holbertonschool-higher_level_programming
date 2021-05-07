ecbb3cf8cb9144f1b257fd70c7d69917e9064891
jj131204 <jj131204@gmail.com>
2021-05-07 02:16:06 +0000

    test

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -5,7 +5,7 @@ def uniq_add(my_list=[]):
     for i in my_list:
         for j in (0, i):
             if my_list[i] == copy[j]:
-                continue
+                break
             else:
                 add = add + i
     return add
