11c55a21c41cd715eed4d5405d0826498125e600
jj131204 <jj131204@gmail.com>
2021-05-07 20:54:15 +0000

    test

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     if a_dictionary:
         max_ = max(a_dictionary, key=a_dictionary.get)
-        return max
+        return max_
     else:
         # max_ = max(a_dictionary, key=a_dictionary.get)
         return None
