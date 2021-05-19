ae4d136cbb3b3d0ad089560779792b7dfaa278a9
jj131204 <jj131204@gmail.com>
2021-05-19 00:30:00 +0000

    first

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""class Square that defines a square by: (based on 0-square.py)"""
+
+
+class Square:
+    """Square"""
+
+    def __init__(self, size):
+
+        self.__size = size
