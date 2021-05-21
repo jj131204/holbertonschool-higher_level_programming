61a936cd972e8e2fc18aeb17bb608af97fac30e9
jj131204 <jj131204@gmail.com>
2021-05-21 18:53:35 +0000

    test

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -4,10 +4,10 @@
 
 class Rectangle:
 
+    """Rectangle"""
+
     number_of_instances = 0
     print_symbol = "#"
-
-    """Rectangle"""
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
