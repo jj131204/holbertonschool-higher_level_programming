e56bce84c08a9c825d8b74f23b0ca686fb8da4c7
jj131204 <jj131204@gmail.com>
2021-05-21 00:27:08 +0000

    pfix

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -20,11 +20,11 @@ class Rectangle:
         elif value < 0:
             raise ValueError("width must be >= 0")
 
-        self._width = value
+        self.__width = value
 
     @property
     def height(self):
-        return self._height
+        return self.__height
 
     @height.setter
     def height(self, value):
@@ -34,4 +34,4 @@ class Rectangle:
         elif value < 0:
             raise ValueError("width must be >= 0")
 
-        self._height = value
+        self.__height = value
