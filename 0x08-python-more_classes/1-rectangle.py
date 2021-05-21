b7cb60be743b4a243c6987a5db9e37223503abd3
jj131204 <jj131204@gmail.com>
2021-05-21 00:24:25 +0000

    public

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,16 +5,13 @@
 class Rectangle:
     """Rectangle"""
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
         return self.__width
 
-    @property
-    def height(self):
-        return self.__height
 
     @width.setter
     def width(self, value):
@@ -25,6 +22,11 @@ class Rectangle:
             raise ValueError("width must be >= 0")
 
         self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
 
     @height.setter
     def height(self, value):
