7efcb5b584a75914ffad9d07526f9652c9a9baeb
jj131204 <jj131204@gmail.com>
2021-05-24 17:49:44 -0500

    test3

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,3 +1,14 @@
 #!/usr/binpython3
 def say_my_name(first_name, last_name=""):
-    
+
+        if type(first_name) != str:
+            raise TypeError("first_name must be a string")
+
+        elif type(last_name) != str:
+            raise TypeError("last_name must be a string")
+
+        elif first_name and last_name:
+            print("My name is {} {}".format(first_name, last_name))
+
+        elif first_name:
+            print("My name is {}".format(first_name))
