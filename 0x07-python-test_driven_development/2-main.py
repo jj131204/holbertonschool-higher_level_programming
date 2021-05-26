d4d51b484079038d532985b441ca12df5608ad6f
jj131204 <jj131204@gmail.com>
2021-05-26 12:44:40 -0500

    test

--- 0x07-python-test_driven_development/2-main.py
+++ 0x07-python-test_driven_development/2-main.py
@@ -3,7 +3,7 @@
 
 matrix_divided = __import__('2-matrix_divided').matrix_divided
 
-matrix = [[3, 8], [12, 11]]
+matrix = [[None]]
 print(matrix_divided(matrix, -3))
 print(matrix)
 
--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -8,12 +8,19 @@ def matrix_divided(matrix, div):
 
     n = len(matrix[0])
 
+    if matrix is None or len(matrix) == 0:
+        raise TypeError("matrix must be a matrix (list of lists) "
+                        "of integers/floats")
+
+
     for i in matrix:
         if len(i) != n:
             raise TypeError("Each row of the matrix must have the same size")
 
     if type(div) != int and type(div) != float:
         raise TypeError("div must be a number")
+
+    
 
     if div is 0:
         raise ZeroDivisionError("division by zero")
