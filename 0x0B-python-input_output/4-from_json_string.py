d74f3d65946492c0809331f0ed2beee464bc25e8
jj131204 <jj131204@gmail.com>
2021-05-31 18:03:38 -0500

    test

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" function that returns an object (Python data structure)
+ represented by a JSON string: """
+import json
+
+
+def from_json_string(my_str):
+    """ returns an object represented by a JSON string: """
+
+    file_ = json.loads(my_str)
