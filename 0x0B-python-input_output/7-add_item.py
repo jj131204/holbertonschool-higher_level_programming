0a281b39450b7a43f03590a1b3755da78d4c4f9d
jj131204 <jj131204@gmail.com>
2021-05-31 19:50:40 -0500

    test

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,11 +7,14 @@ from sys import argv
 write = __import__('5-save_to_json_file').save_to_json_file
 create = __import__('6-load_from_json_file').load_from_json_file
 
-
 if __name__ == "__main__":
+    """
+    # Adds all arguments to a Python list, and then save them to a file.
+    """
 
     new_list = []
 
+    """ Check if file exists """
     if path.isfile("add_item.json"):
         new_list = create("add_item.json")
 
@@ -23,4 +26,5 @@ if __name__ == "__main__":
     """
     new_list.append(argv[1:])
 
+    """ Write the list with the new elements in the file """
     write(new_list, "add_item.json")
