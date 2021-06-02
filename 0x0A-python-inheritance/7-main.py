861b43040b9a7a4762426c97e533abfafcee9aea
jj131204 <jj131204@gmail.com>
2021-06-02 18:43:43 -0500

    fix

--- 0x0A-python-inheritance/7-main.py
+++ 0x0A-python-inheritance/7-main.py
@@ -20,3 +20,9 @@ try:
     bg.integer_validator("distance", -4)
 except Exception as e:
     print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    bg.integer_validator("zero", 0)
+
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e)) 
