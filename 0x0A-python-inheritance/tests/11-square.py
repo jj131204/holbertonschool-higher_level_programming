0a1aa61ef93e4953acc621f0dc38e6d884d29943
jj131204 <jj131204@gmail.com>
2021-06-02 19:55:42 -0500

    test

---
+++ 0x0A-python-inheritance/tests/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Square Class """
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    # Defines a Square.
+    Args:
+        size (int) = Size of one side of the square.
+    """
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
