568ac574f7f71818232135b730471c7bdacde688
jj131204 <jj131204@gmail.com>
2021-06-07 18:00:28 -0500

    point5

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -98,3 +98,10 @@ class Rectangle(Base):
                 print("#", end="")
 
             print()
+
+    """point 5 """
+    def __str__(self):
+        str_ = "[Rectangle] ({}) <{}>/<{}> - <{}>/{}>" .format(
+            self.id, self.__x, self.__y, self.__width, self.__height)
+
+        return str_
