4e2f60a4063f9fbe4c4b8e1ba6a8f7b4dd3c3dae
jj131204 <jj131204@gmail.com>
2021-06-08 13:57:18 -0500

    __str__

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -110,7 +110,7 @@ class Rectangle(Base):
     def __str__(self):
         """method so that it returns [Rectangle] (<id>) <x>/<y>
          - <width>/<height>"""
-        str_ = "[Rectangle] ({}) <{}>/<{}> - <{}>/{}>" .format(
+        str_ = "[Rectangle] ({}) {}/{} - {}/{}" .format(
             self.id, self.__x, self.__y, self.__width, self.__height)
 
         return str_
