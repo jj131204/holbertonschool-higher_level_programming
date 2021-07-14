89679e6fdd04ef4ed49858cb3245cd6054dff918
jj131204 <jj131204@gmail.com>
2021-07-14 18:31:10 -0500

    test

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,3 @@
+-- lists all genres from hbtn_0d_tvshows 
+SELECT g.name "genre", COUNT(sg.genre_id) "number_of_shows" FROM tv_genres g JOIN tv_show_genres sg ON g.id = sg.genre_id GROUP BY sg.genre_id ORDER BY COUNT(sg.genre_id)  DESC;
+
