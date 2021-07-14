36d7ea2504aa37604a9d1e9b360efd71e7de9ce4
jj131204 <jj131204@gmail.com>
2021-07-13 22:37:38 -0500

    test1

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,7 +2,18 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(
-	`id` INT UNIQUE AUTO_INCREMENT NOT NULL,
-	`state_id` INT NOT NULL,`name` VARCHAR(256),
-	PRIMARY KEY(`id`),
+	id INT UNIQUE AUTO_INCREMENT NOT NULL,
+	state_id INT NOT NULL,
+	name VARCHAR(256),
+	PRIMARY KEY(id),
 	FOREIGN KEY(`state_id`) REFERENCES states(`id`));
+
+--CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+--CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
+--	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--	state_id INT,
+--	name VARCHAR(256) NOT NULL,
+--       	FOREIGN KEY (state_id) REFERENCES states(id)
+--);
+
+
