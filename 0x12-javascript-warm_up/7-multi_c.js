0eed33f893d1f08b770d8756c41571dcf83e8f39
jj131204 <jj131204@gmail.com>
2021-08-02 20:55:07 -0500

    c is fun

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,6 +3,10 @@
 const myVar = 'C is fun';
 const arg = parseInt(process.argv[2]);
 let count;
-for (count = 0; count <= arg; count++) {
-  console.log(myVar);
+if (isNaN(arg)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (count = 0; count <= arg; count++) {
+    console.log(myVar);
+  }
 }
