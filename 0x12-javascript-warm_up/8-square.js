d4da1a4c66cdad101e8c76e7fa8dc06aaff57cc3
jj131204 <jj131204@gmail.com>
2021-08-02 21:23:41 -0500

    X

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,10 +5,8 @@ const arg = parseInt(process.argv[2]);
 let count;
 if (isNaN(arg)) {
   console.log('Missing size');
-}
-else {
+} else {
   for (count = 0; count < arg; count++) {
     console.log(myVar.repeat(arg));
-
   }
 }
