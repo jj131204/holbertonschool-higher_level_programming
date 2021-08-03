43a84e0f4633325ffb62de4ecad993feb85febb9
jj131204 <jj131204@gmail.com>
2021-08-03 17:27:08 -0500

    test

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+// square
+const Rectangle = require('./5-square');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+
+    let count;
+    for (count = 0; count < this.height; count++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+module.exports = Square;
