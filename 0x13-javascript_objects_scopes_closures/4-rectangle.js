b6608b83b6ef9c0482aeecdba854e213fb2380f9
jj131204 <jj131204@gmail.com>
2021-08-03 15:03:04 -0500

    class

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -7,11 +7,22 @@ class Rectangle {
       this.height = h;
     }
   }
+
   print () {
     let count;
     for (count = 0; count < this.height; count++) {
-       console.log('X'.repeat(this.width));
+      console.log('X'.repeat(this.width));
     }
+  }
+
+  rotate () {
+    this.width = this.height;
+    this.height = this.width;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
   }
 }
 module.exports = Rectangle;
