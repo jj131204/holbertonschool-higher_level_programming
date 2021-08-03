e0fe9ccfa23bdaca78c85ab166325013109d6213
jj131204 <jj131204@gmail.com>
2021-08-03 18:13:01 -0500

    8

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+  let x;
+  const array = [];
+  for (x = list.length - 1; x > 0; x--) {
+    array.push(list[x]);
+  }
+
+  return (array);
+};
