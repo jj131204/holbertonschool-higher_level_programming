e5534cde0094e56ff48228f5cec589e42c7a55d3
jj131204 <jj131204@gmail.com>
2021-08-02 20:30:51 -0500

    parseInt

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,7 +3,6 @@
 const myVar = parseInt(process.argv[2]);
 if (isNaN(myVar)) {
   console.log('Not a number');
-}
-else {
-  console.log('My number: '+ myVar);
+} else {
+  console.log('My number: ' + myVar);
 }
