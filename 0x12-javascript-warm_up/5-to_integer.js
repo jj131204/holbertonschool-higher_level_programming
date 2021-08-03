2004b06645897510d3060ccf4b2133a147cc2669
jj131204 <jj131204@gmail.com>
2021-08-02 20:39:28 -0500

    Array

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,6 +3,7 @@
 const myVar = parseInt(process.argv[2]);
 if (isNaN(myVar)) {
   console.log('Not a number');
-} else {
-  console.log('My number: ' + myVar);
+}
+else {
+  console.log('My number: '+ myVar);
 }
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// Array
+const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let count;
+for (count = 0; count < 3; count++) {
+  console.log(myVar[count]);
+}
