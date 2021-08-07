e4f783daefb669226c9e4f84bf63073627857a2d
jj131204 <jj131204@gmail.com>
2021-08-07 12:41:19 -0500

    test1

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """test1"""
-if __name__ == "__main__":
+if __name__ == '__main__':
     """main"""
 
     import MySQLdb
-    import sys
+    from sys import argv
 
-    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], password=sys.argv[2],  db=sys.argv[3])
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=argv[1], password=argv[2], db=argv[3])
 
     cursor = db.cursor()
 
