e2880ebfe304658aa817734e19d65a4428c0476a
jj131204 <jj131204@gmail.com>
2021-08-07 15:06:13 -0500

    first

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,8 +10,7 @@ if __name__ == '__main__':
                            user=argv[1], passwd=argv[2], db=argv[3])
 
     cur = conn.cursor()
-    cur.execute("SELECT name FROM states WHERE name LIKE 'N%'
-                ORDER BY states.id ASC")
+    cur.execute("""SELECT name FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC""")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
