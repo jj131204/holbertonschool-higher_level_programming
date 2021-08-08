d0160d2393b458b2e68d05aaee6d5c399d94c5f7
jj131204 <jj131204@gmail.com>
2021-08-08 18:54:41 +0000

    test3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
-""""""
+""" write a script that takes in arguments and displays
+    all values in the states table of hbtn_0e_0_usa
+"""
 if __name__ == '__main__':
 
     import MySQLdb
