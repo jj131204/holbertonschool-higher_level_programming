936f4f472948f9cd1c0011fa439389a7641ae1da
jj131204 <jj131204@gmail.com>
2021-08-12 15:55:43 +0000

    .

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,15 +1,24 @@
 #!/usr/bin/python3
-"""First state model."""
+"""
+This module has one class:
+    City - inherited from Base.
+    """
 
-from sqlalchemy import Column, Integer, String
+
+from model_state import State
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 Base = declarative_base()
 
 
-class State(Base):
-    """ state class """
-    __tablename__ = 'states'
-    id = Column(Integer, nullable=False, autoincrement="auto",
-                primary_key=True)
-    name = Column(String(128), nullable=False)
+class City(Base):
+    """
+        city
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128),
+                  nullable=False)
+    state_id = Column(Integer,
+                      ForeignKey(State.id))
