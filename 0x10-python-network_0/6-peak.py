0ae01f15df42250416741b14ec336dc90f734513
jj131204 <jj131204@gmail.com>
2021-08-20 22:27:28 +0000

    ppep8

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """ python """
 
-def find_peak(list_of_integers):
-    """peak"""
 
+def find_peak(list_of_integers):
+    """ peak """
 
     if (list_of_integers):
         list_of_integers.sort()
         peak_ = list_of_integers
-
         return peak_[-1]
 
     else:
