641aa15b6634a18ba40e42a02624d5cb88856ed4
jj131204 <jj131204@gmail.com>
2021-08-21 02:39:04 +0000

    7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""python"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    """ ...  """
+
+    url = requests.get(argv[1])
+    if url.status_code >= 400:
+        print('Error code: {}'.format(r.status_code))
+    else:
+        print(url.text)
