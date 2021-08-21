a8ec87b06c2ddd9638ff77032444a19c40e2be46
jj131204 <jj131204@gmail.com>
2021-08-21 02:00:13 +0000

    requests

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""python"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    """ ...  """
+    url = requests.get(argv[1])
+
+    print(url.headers['X-Request-Id'])
