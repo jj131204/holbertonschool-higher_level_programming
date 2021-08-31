abe7e3533d15fe968fe1fd8699df43632b4a9e8d
root <root@LAPTOP-4MQ15EMS.localdomain>
2021-08-31 18:17:31 -0500

    test

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-curl -s -o /dev/null -w "%{http_code}" "$1"
+# Send JSON POST request to a URL passed as the first argument, and displays the body of the response.
+curl -s -H "Content-Type: application/json" -d @"$2" "$1"
