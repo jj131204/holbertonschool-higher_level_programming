3cdde6a610223654d36da00b945c4a31b11821e9
jj131204 <jj131204@gmail.com>
2021-09-14 23:06:22 +0000

    fix

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -6,7 +6,7 @@ const argv1 = process.argv[2];
 const https = require('https');
 
 https.get(argv1, function (res) {
-  console.log('code: ', res.statusCode);
+  console.log('code:', res.statusCode);
 }).on('error', function (e) {
   console.error(e);
 });
