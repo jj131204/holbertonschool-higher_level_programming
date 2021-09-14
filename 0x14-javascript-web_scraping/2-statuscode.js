525a42110de47ee1a8094af19fa0aae90bce9592
jj131204 <jj131204@gmail.com>
2021-09-14 23:37:37 +0000

    test

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
 //  reads and prints the content of a file
 
-const argv1 = process.argv[2];
 
-const https = require('https');
+const url = process.argv[2];
+const request = require('request');
 
-https.get(argv1, function (res) {
-  console.log('code:', res.statusCode);
-}).on('error', function (e) {
-  console.error(e);
+request(url, function (err, result, body) {
+  console.log('code: ' + result.statusCode);
+  if (err) {
+    console.log(err);
+  }
 });
