464066cb696694c2812350487ba30ea3e0cb2338
jj131204 <jj131204@gmail.com>
2021-09-14 22:59:57 +0000

    semiestandar

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+//  reads and prints the content of a file
+
+const argv1 = process.argv[2];
+
+const https = require('https');
+
+https.get(argv1, function (res) {
+  console.log('code: ', res.statusCode);
+}).on('error', function (e) {
+  console.error(e);
+});
