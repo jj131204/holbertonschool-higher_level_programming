f9f8e47b2bda175f956e6806a5dedb45eba6b1ca
jj131204 <jj131204@gmail.com>
2021-09-15 00:07:25 +0000

    test

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,10 +2,12 @@
 //  reads and prints the content of a file
 
 const argv = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + argv;
+
 const request = require('request');
 
-request(argv, function (err, result, body) {
-  console.log('code: ' + result.statusCode);
+request(url, function (err, result, body) {
+  console.log(JSON.parse(body).title);
   if (err) {
     console.log(err);
   }
