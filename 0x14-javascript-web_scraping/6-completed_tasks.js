9481c6f1b528d12f98ebc1f9e33c8b1168700946
jj131204 <jj131204@gmail.com>
2021-09-15 03:34:49 +0000

    test

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const request = require('request');
+const argv1 = process.argv[2];
+
+request(argv, function (err, res, body) {
+  if (err) {
+    console.log(err);
+  }
+
+  const data = JSON.parse(body);
+  const taskCompleted = {};
+
+  for (const task of data) {
+    if (task.completed === true) {
+      if (task.userId in taskCompleted) {
+        taskCompleted[task.userId] += 1;
+      } else {
+        taskCompleted[task.userId] = 1;
+      }
+    }
+  }
+  console.log(taskCompleted);
+});
